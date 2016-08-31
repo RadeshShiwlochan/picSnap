4d05c990cc4d31d6a9280ad3c9c3a03801d576a0
radeshshiwlochan <radesh0430@gmail.com>
2016-08-30 23:11:40 -0400

    serves a home page

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+	path    = require('path'),
+	bodyParser = require('body-parser'),
+	app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res) {
+	res.render('home.ejs');
+});	
+
+app.listen(3000, function() {
+	console.log("App running on port:3000");
+});
